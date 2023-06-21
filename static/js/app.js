916fff04c86d4787c122fb870c6cd9a1d7cfc9e8
m-janssens-boop <127706155+m-janssens-boop@users.noreply.github.com>
2023-06-21 14:02:09 -0700

    building metadata function

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,82 @@
-// read in url
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// promise pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
 
-//fetch the JSON data and console log it
-d3.json(url).then(function(data){
+//function buildMetadata(sample) {
+
+    // Access the website and use d3 to operate on the data
+    // read in url
+    const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+    // promise pending
+    const dataPromise = d3.json(url);
+    console.log("Data Promise: ", dataPromise);
+
+    //fetch the JSON data and console log it
+    d3.json(url).then(function(data){
     console.log(data);
-});
+    });
+  
+    // Filter the data for the object with the desired sample number (the id)
+  
+  
+    // Select the panel with id of `#sample-metadata`
+      
+  
+    // Clear existing metadata - use `.html("")`
+      
+  
+    // Append new tags for each key-value in the metadata
+  
+  
+    // If you want to do the bonus, you can make the gauge chart here
+  
+//} 
+    ;
+
+  
+  function buildCharts(sample) {
+    // Access the website and use .then to operate on the data
+  
+  
+      // Filter the data for the object with the desired sample number (the id)
+  
+  
+      // Pull the desired information (ids, labels, values) from your filtered data
+  
+  
+      // Build a Bubble Chart
+      
+  
+      // Slice the data for your bar chart and order it (you can just use reverse)
+  
+  
+      // Build a Horizontal Bar Chart
+  
+  
+    ;
+  }
+  
+  function init() {
+    // Get the reference to the dropdown menu
+  
+  
+    // Use the list of sample names to populate the select options
+    // Do this by pulling the array associated with `names` 
+      
+  
+      // Loop through the names and append to the dropdown menu
+  
+  
+      // Use the first sample from the list to build the initial plots
+      
+  
+    ;
+  }
+  
+  function optionChanged(newSample) {
+    // Change your data and update your plots/metadata when newSample is selected from the dropdown
+  
+  
+  }
+  
+  // Initialize the dashboard
+  init();
